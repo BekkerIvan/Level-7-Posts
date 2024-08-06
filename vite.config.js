352b9a8f9ad609a9f478e8fb9add6d25a0724594
Ivan-Bekker <ivan.bekker@stratusolve.com>
2024-08-06 18:28:23 +0200

    Initial Commit 9

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // or 'localhost'
+        port: 5173,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
